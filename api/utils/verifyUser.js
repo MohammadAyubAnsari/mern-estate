3316a65c1bb169d0797d2809c820fc037952805f
Mohammad Ayub Ansari <119043948+MohammadAyubAnsari@users.noreply.github.com>
2023-10-20 17:07:07 +0530

    Completed update user functionality

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,19 +1,3 @@
-// import jwt from "jsonwebtoken";
-// export const verifyToken = (req, res, next) => {
-//   const token = req.cookie.accesstoken;
-
-//   if (!token) {
-//     return next(errorHandler(401, "Unauthorized"));
-//   }
-
-//   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-//     if (err) return next(errorHandler(403, "Forbidden"));
-
-//     req.user = user;
-//     next();
-//   });
-// };
-
 import jwt from "jsonwebtoken";
 import { errorHandler } from "./error.js";
 
