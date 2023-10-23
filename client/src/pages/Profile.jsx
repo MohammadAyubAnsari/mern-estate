7e9776cd74c8a788c27b81fb0991931729655066
MohammadAyubAnsari <mdayubansari2014@gmail.com>
2023-10-23 14:37:03 +0530

    Completed delete user listing  functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -147,6 +147,26 @@ const Profile = () => {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -260,7 +280,12 @@ const Profile = () => {
                 <p>{listing.name}</p>
               </Link>
               <div className="flex flex-col items-center">
-                <button className="text-red-700 uppercase">DELETE</button>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  DELETE
+                </button>
                 <button className="text-green-700 uppercase">EDIT</button>
               </div>
             </div>
